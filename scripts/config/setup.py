9e642c99856cf92d7edfa1ea967b667ad75ac3df
suntianfengg <suntianfeng@huawei.com>
2026-07-23 20:22:40 +0800

    fix(whl packaging): mark FunctionSystem wheel as non-pure Set Root-Is-Purelib to false so FunctionSystem package files are installed   through the platform-specific library path.
    Signed-off-by: suntianfengg <suntianfeng@huawei.com>

---
+++ scripts/config/setup.py
@@ -0,0 +1,25 @@
+# coding=UTF-8
+# Copyright (c) 2026 Huawei Technologies Co., Ltd
+from setuptools import Distribution, setup
+from setuptools.command.bdist_wheel import bdist_wheel as _bdist_wheel
+
+
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
+
+
+class FunctionSystemBdistWheel(_bdist_wheel):
+    def finalize_options(self):
+        super().finalize_options()
+        self.root_is_pure = False
+
+    def get_tag(self):
+        _, _, platform_tag = super().get_tag()
+        return "py3", "none", platform_tag
+
+
+setup(
+    distclass=BinaryDistribution,
+    cmdclass={"bdist_wheel": FunctionSystemBdistWheel},
+)
